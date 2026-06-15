535d4bbe927158342a4ba1ca83de767b0e39dc44
fabiocofa <fabio.cofa@gmail.com>
2026-06-15 12:27:23 +0200

    Update frasi.js

--- frasi.js
+++ frasi.js
@@ -70,5 +70,9 @@ testo: "Molto spesso non ci manca la risposta. Ci manca il coraggio di accettarl
 domanda: "Quale verità stai evitando di guardare in questo momento?"
 }
 
+const giorno = Math.floor(Date.now() / 86400000);
+
+const contenuto = frasi[giorno % frasi.length];
+
 ];
 
